2b3a8d4013047faed98df2e39c0602332d66f618
Patrick Smith <pgwsmith@gmail.com>
2015-09-14 23:01:40 +1000

    Export composable functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 export { default as default } from './Flambeau';
+export { default as connectActionSets } from './connectActionSets';
+export { default as createResourcesFromReducers } from './createResourcesFromReducers';
+export { default as dispatch } from './dispatch';
+export { default as getConsensus } from './getConsensus';
