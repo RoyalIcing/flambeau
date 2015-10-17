fc2d5984ad0e7476947c83ea64109148e507829a
Patrick Smith <pgwsmith@gmail.com>
2015-10-17 22:31:38 +1100

    combineReducers() now accepts on demand getPropsByID()

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,89 +1,96 @@
 import { GET_INITIAL_STATE } from './types';
 
-export default function combineReducers(reducers, { idToProps = {}, alsoAdd } = {}) {
-  let combinedReducer = {};
+
+export default function combineReducers(reducers, { getPropsByID = () => {}, alsoAdd } = {}) {
 	let alsoResponder;
+	const reducerIDs = Object.keys(reducers);
 
-  function getInitialState() {
-    return Object.keys(reducers).reduce((state, reducerID) => {
-      const reducer = reducers[reducerID];
-      if (reducer[GET_INITIAL_STATE]) {
-        state[reducerID] = reducer[GET_INITIAL_STATE](idToProps[reducerID]);
-      }
-      return state;
-    }, {});
-  }
-
-  const actionSetHandlers = Object.keys(reducers).reduce((actionSetHandlers, reducerID) => {
-  const reducer = reducers[reducerID];
-  Object.keys(reducer).reduce((actionSetHandlers, property) => {
-    if (property === GET_INITIAL_STATE || !!actionSetHandlers[property]) {
-      return actionSetHandlers;
-    }
-
-    actionSetHandlers[property] = (initialState, { isAction, isIntrospection, forwardTo }) => {
-      if (isAction) {
-        let newState = Object.keys(reducers).reduce((state, reducerID) => {
-          state[reducerID] = forwardTo({
-            responder: reducers[reducerID],
-            initialState: initialState[reducerID],
-            props: idToProps[reducerID]
-          });
-          return state;
-        }, {});
-
-        if (alsoResponder) {
-  				newState = forwardTo({
-  					responder: alsoResponder,
-  					initialState: newState,
-  					props: {
-  						idToProps,
-  						combinedReducer
-  					}
-  				});
-  			}
-
-        return newState;
-      }
-      else if (isIntrospection) {
-				if (alsoResponder) {
-					// Use customized response, if one was given.
-					const response = forwardTo({
-						responder: alsoResponder,
-						initialState: newState,
-						props: {
-							idToProps,
-							combinedReducer
-						}
-					});
-
-					if (response) {
-						return response;
-					}
+	let combinedReducer = {
+		[GET_INITIAL_STATE]: (masterProps) => {
+			const propsByID = getPropsByID(masterProps);
+			return reducerIDs.reduce((state, reducerID) => {
+				const reducer = reducers[reducerID];
+				if (reducer[GET_INITIAL_STATE]) {
+					state[reducerID] = reducer[GET_INITIAL_STATE](propsByID[reducerID]);
 				}
+				return state;
+			}, {});
+		}
+	};
 
-				// Use first reducer that responds.
-				return Object.keys(reducers).find((state, reducerID) =>
-					forwardTo({
-						responder: reducers[reducerID],
-						initialState: initialState[reducerID],
-						props: idToProps[reducerID]
-					})
-				);
+	const actionSetHandlers = reducerIDs.reduce((actionSetHandlers, reducerID) => {
+		const reducer = reducers[reducerID];
+		Object.keys(reducer).reduce((actionSetHandlers, property) => {
+			if (property === GET_INITIAL_STATE || !!actionSetHandlers[property]) {
+				return actionSetHandlers;
 			}
-    }
 
-    return actionSetHandlers;
-  }, actionSetHandlers);
+			actionSetHandlers[property] = (initialState, { isAction, isIntrospection, props, forwardTo }) => {
+				const propsByID = getPropsByID(props);
+				if (isAction) {
+					let newState = reducerIDs.reduce((state, reducerID) => {
+						state[reducerID] = forwardTo({
+							responder: reducers[reducerID],
+							initialState: initialState[reducerID],
+							props: propsByID[reducerID]
+						});
+						return state;
+					}, {});
 
-  return actionSetHandlers;
-  }, {});
+					if (alsoResponder) {
+						newState = forwardTo({
+							responder: alsoResponder,
+							initialState: newState,
+							props: {
+								props,
+								propsByID
+							},
+							sourceResponder: combinedReducer
+						});
+					}
 
-  Object.assign(combinedReducer, { [GET_INITIAL_STATE]: getInitialState }, actionSetHandlers);
+					return newState;
+				}
+				else if (isIntrospection) {
+					if (alsoResponder) {
+						// Use customized response, if one was given.
+						const response = forwardTo({
+							responder: alsoResponder,
+							initialState: newState,
+							props: {
+								props,
+								propsByID
+							},
+							sourceResponder: combinedReducer
+						});
 
-  if (alsoAdd) {
+						if (response) {
+							return response;
+						}
+					}
+
+					// Use first reducer that responds.
+					return reducerIDs.find((state, reducerID) =>
+						forwardTo({
+							responder: reducers[reducerID],
+							initialState: initialState[reducerID],
+							props: propsByID[reducerID]
+						})
+					);
+				}
+			}
+
+			return actionSetHandlers;
+		}, actionSetHandlers);
+
+		return actionSetHandlers;
+	}, {});
+
+	Object.assign(combinedReducer, actionSetHandlers);
+
+	if (alsoAdd) {
 		alsoResponder = alsoAdd(combinedReducer);
 	}
 
-  return combinedReducer;
+	return combinedReducer;
 }
