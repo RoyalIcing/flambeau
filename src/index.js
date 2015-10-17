af290d4bce783e0edf3a1dc13d1b7d8c25514e91
Patrick Smith <pgwsmith@gmail.com>
2015-10-17 23:32:08 +1100

    Make built-in Store secondary

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as default } from './Flambeau';
 export { default as connectActionSets } from './connectActionSets';
 export { default as createResourcesFromReducers } from './createResourcesFromReducers';
 export { default as dispatch } from './dispatch';
