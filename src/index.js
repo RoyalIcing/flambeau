74c416413a385c908388066d8b481259e8491583
Patrick Smith <pgwsmith@gmail.com>
2015-10-27 00:17:39 +1100

    Remove old resources-based functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 export { default as combineReducers } from './combineReducers';
-export { default as createResourcesFromReducers } from './createResourcesFromReducers';
-export { default as dispatch } from './dispatch';
 export { default as getConsensus } from './getConsensus';
 export { default as connectActionSets } from './connectActionSets';
